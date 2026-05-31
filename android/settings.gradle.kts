b92e500d105afb03dc82b0ae1b5d57757a890292
Hari Raval <hariraval81@gmail.com>
2026-05-31 12:38:22 +0530

    feat: implement GoRouter navigation configuration and update Android settings.gradle to KTS

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,7 +22,6 @@ plugins {
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
     // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.android") version "2.3.10" apply false
 }
 
